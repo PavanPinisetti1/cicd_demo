3d1f69f0bff74b0b94f3eedb5115e52c86f4ac6c
Pavan Pinisetti <pavan.pinisetti@winfosolutions.com>
2025-08-19 15:20:58 +0530

    Initial export with Application 201 and CICD_DEPT table

---
+++ src/database/wksp_ebs2cloud_migration/tables/cicd_dept.sql
@@ -0,0 +1,8 @@
+create table wksp_ebs2cloud_migration.cicd_dept (
+    id    number,
+    dname varchar2(100 byte),
+    loc   varchar2(50 byte)
+);
+
+
+-- sqlcl_snapshot {"hash":"349df1fefa7811501f82cacd598ea1368c340920","type":"TABLE","name":"CICD_DEPT","schemaName":"WKSP_EBS2CLOUD_MIGRATION","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>WKSP_EBS2CLOUD_MIGRATION</SCHEMA>\n   <NAME>CICD_DEPT</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>ID</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>DNAME</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>100</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>LOC</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>50</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n   </RELATIONAL_TABLE>\n</TABLE>"}
